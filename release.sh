e413d5419d3563aed2761bf051c7ca1553b4bafd
David Lechner <david@lechnology.com>
2017-03-24 15:57:38 -0500

    Change jessie > stretch in release script

--- release.sh
+++ release.sh
@@ -7,10 +7,10 @@ set -e
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 
-OS=debian DIST=jessie ARCH=armel pbuilder-ev3dev build
+OS=debian DIST=stretch ARCH=armel pbuilder-ev3dev build
 
-debsign ~/pbuilder-ev3dev/debian/jessie-armel/${source}_${version}_armel.changes
+debsign ~/pbuilder-ev3dev/debian/stretch-armel/${source}_${version}_armel.changes
 
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/jessie-armel/${source}_${version}_armel.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/stretch-armel/${source}_${version}_armel.changes
 
 gbp buildpackage --git-tag-only
