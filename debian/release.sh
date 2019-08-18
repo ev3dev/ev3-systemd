f3e7fbc69b819bd7b8e3787ceee748a121b6e8c4
David Lechner <david@lechnology.com>
2019-08-18 15:09:28 -0500

    debian/release.sh: no longer using codename for uploads

--- debian/release.sh
+++ debian/release.sh
@@ -7,12 +7,11 @@ set -e
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
-codename=$(debian-distro-info --codename --${distribution})
 
-OS=debian DIST=${codename} ARCH=armel pbuilder-ev3dev build
+OS=debian DIST=${distribution} ARCH=armel pbuilder-ev3dev build
 
-debsign ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
+debsign ~/pbuilder-ev3dev/debian/${distribution}-armel/${source}_${version}_armel.changes
 
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${distribution}-armel/${source}_${version}_armel.changes
 
 gbp buildpackage --git-tag-only
